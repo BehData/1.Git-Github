b156ad1e5c436d3c7a8cf4e2ebc40c6b987a94f2
JackelineGS <jackeline.garcia1@unmsm.edu.pe>
2022-06-18 06:22:06 -0500

    Update

--- Primer-codigo.R
+++ Primer-codigo.R
@@ -1,3 +1,20 @@
 #HOLA MUNDO
 VAR <- 3456
-RT <- 14582
+
+install.packages("sp")
+install.packages("rgdal")
+install.packages("ggplot2")
+install.packages("sf")
+install.packages("purrr")
+install.packages("ggrepel")
+install.packages("sf")
+install.packages("scales")
+
+pacman::p_load(tidyverse, openxlsx, labelled, ggplot2,scales, jcolors)
+
+# Importar data
+
+BASE <- readxl::read_xlsx("Base.xlsx")
+BASE_B <- BASE %>% 
+  filter(Grade == "Pregrado") %>% 
+  count(Region)
